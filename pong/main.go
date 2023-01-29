5d2c1fcda4f047aeb9b6c329be8cb4bc2f6ce203
Rwik Mukhopadhyay <rwikee@gmail.com>
2023-01-29 21:55:48 +0530

    Added ping output as animation object

--- pong/main.go
+++ pong/main.go
@@ -1,12 +1,24 @@
 // main.go
 package main
 
-import "os"
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
 	ipAddr := "*"
 	if len(os.Args) >= 2 {
 		ipAddr = os.Args[1]
 	}
-	start(ipAddr)
+
+	out, _ := exec.Command("ping", ipAddr).Output()
+	if strings.Contains(string(out), "Request timed out") {
+		start("IT'S DOWN")
+	} else {
+		start("IT'S ALIVEEE")
+	}
+	//fmt.Println(string(out))
+	//start(ipAddr)
 }
